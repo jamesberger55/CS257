1c4d3a12911dcd9aa548434c7d67b6ed8a3ae3cc
James Berger <jamesberger@Jamess-MacBook-Pro.local>
2022-09-23 09:12:25 -0500

    changed order of books in tinycsv test

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -27,9 +27,9 @@ class BooksDataSourceTester(unittest.TestCase):
         tiny_data_source = BooksDataSource('tinybooks.csv')
         books = tiny_data_source.books()
         self.assertTrue(len(books) == 3)
-        self.assertTrue(books[0].title == 'Emma')
-        self.assertTrue(books[1].title == 'Neverwhere')
-        self.assertTrue(books[2].title == 'Omoo')
+        self.assertTrue(books[0].title == 'Omoo')
+        self.assertTrue(books[1].title == 'Emma')
+        self.assertTrue(books[2].title == 'Neverwhere')
     
 
 if __name__ == '__main__':
