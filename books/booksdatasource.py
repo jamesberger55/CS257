3e1c61f948d832171cd3f56930acbc3ca6e62c2d
James Berger <62909049+jamesberger55@users.noreply.github.com>
2022-09-28 21:06:44 -0500

    Update booksdatasource.py

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -64,22 +64,22 @@ class BooksDataSource:
             author_facts = row[2].replace(" (", ",").replace(")", "").replace("-", ",").replace("\n",'').replace(' and ', ',')
             author_list = author_info.split(",")
             k = 0
-           
-        for i in range (len(author_info)//3):
-            author_name = author_info[k]
-            author_birth = int(author_info[k+1])  
-            author_death = author_info[k+2]
-            if author_death == '':
-               author_death = Null
-            else:
-            author_death = int(author_death)
+              
+            for i in range (len(author_info)//3):
+                author_name = author_info[k]
+                author_birth = int(author_info[k+1])  
+                author_death = author_info[k+2]
+                if author_death == '':
+                    author_death = Null
+                else:
+                    author_death = int(author_death)
                          
-        author_names_dict = author_name.split(" ")
-        author_first = author_names_dict[0]
-        author_last = author_names_dict[1]
-        author = Author(surname=author_last, given_name=author_first, birth_year=author_birth, death_year=author_death)
+            author_names_dict = author_name.split(" ")
+            author_first = author_names_dict[0]
+            author_last = author_names_dict[1]
+            author = Author(surname=author_last, given_name=author_first, birth_year=author_birth, death_year=author_death)
                 
-        author_list.append(author)
+            author_list.append(author)
                                
         self.csv = books_csv_file_name
         book_list = []
