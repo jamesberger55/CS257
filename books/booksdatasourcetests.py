91f5bc808424170c1fd4f65bb37e966d434044bf
James Berger <62909049+jamesberger55@users.noreply.github.com>
2022-09-30 10:21:58 -0500

    Update booksdatasourcetests.py

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -61,6 +61,9 @@ class BooksDataSourceTester(unittest.TestCase):
    
     def test_case_sensitive_title(self):
         books = self.data_source.books('Beloved')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('Beloved'))
+        print("1")
   
     def test_case_sensitive_title(self):
         books = self.data_source.books('beloved')
