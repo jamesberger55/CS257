8265a1a70e4cfa1796681de85212ed8d8bfaf368
James Berger <62909049+jamesberger55@users.noreply.github.com>
2022-09-30 10:19:26 -0500

    Create books.py

---
+++ books/books.py
@@ -0,0 +1,49 @@
+def authors_display(data_source):
+    if (len(sys.argv) == 2):
+       results = data_source.authors()
+    else:
+       results = data_source.authors(sys.argv[2])
+    for author in results:
+       print(author)
+ 
+def books_display(data_source)
+    if (len(sys.argv) == 2):
+        results = data_source.books()
+    elif (len(sys.argv) == 3): 
+        results = data_source.books(sys.argv[2])
+    else:
+        results = data_source.books(sys.argv[2], sys.argv[3])
+    for book in results:
+        print(book)
+
+def year_display(data_source):
+    if (len(sys.argv) == 2):
+        results = data_source.books_between_years()
+    elif (len(sys.argv) == 3): 
+        results = data_source.books_between_years(sys.argv[2])
+    else:\
+        results = data_source.books_between_years(sys.argv[2], sys.argv[3])
+    for book in results:
+        print(book)
+
+
+def main():
+    data_source = booksdatasource.BooksDataSource('books1.csv')
+    
+    if (len(sys.argv) <=1):
+       print("input error")
+    
+    elif (len(sys.argv) <= 3):
+            display_authors(data_source)
+        
+    elif (len(sys.argv) <= 4):
+            display_books(data_source)
+
+    elif (len(sys.argv) <= 4):
+            display_years(data_source)
+    
+    else: 
+         print("error")
+       
+if __name__ == '__main__':
+    main()
