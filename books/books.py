72b4e56f4e58eb24288539a7ae0e56dd70d78e0d
Doug <phamd@carleton.edu>
2022-09-30 14:09:29 -0500

    fixed bug in books.py

--- books/books.py
+++ books/books.py
@@ -24,7 +24,7 @@ def year_display(data_source):
         results = data_source.books_between_years()
     elif (len(sys.argv) == 3): 
         results = data_source.books_between_years(sys.argv[2])
-    else:\
+    else:
         results = data_source.books_between_years(sys.argv[2], sys.argv[3])
     for book in results:
         print(book)
@@ -32,21 +32,62 @@ def year_display(data_source):
 
 def main():
     data_source = booksdatasource.BooksDataSource('books1.csv')
-    
+    '''
     if (len(sys.argv) <=1):
        print("input error")
     
     elif (len(sys.argv) <= 3):
-            display_authors(data_source)
+            authors_display(data_source)
         
     elif (len(sys.argv) <= 4):
-            display_books(data_source)
+            books_display(data_source)
 
     elif (len(sys.argv) <= 4):
-            display_years(data_source)
+            year_display(data_source)
     
     else: 
          print("error")
+    '''
+
+    if (len(sys.argv) < 2):
+        print("More arguments required, please see man to see command line interface")
+    elif sys.argv[1] == "title":
+        if len(sys.argv) == 3:
+            books = data_source.books(sys.argv[2])
+            print(books)
+        elif len(sys.argv) == 2:
+            books = data_source.books()
+            print(books)
+        else:
+            print("Too many arguments provided, please see man to see command line interface")
+    elif sys.argv[1] == "author":
+        if len(sys.argv) == 3:
+            authors = data_source.authors(sys.argv[2])
+            print(authors)
+        elif len(sys.argv) == 2:
+            authors = data_source.authors()
+            print(authors)
+        else:
+            print("Too many arguments provided, please see man to see command line interface")
+    elif sys.argv[1] == "years":
+        if len(sys.argv) == 4:
+            years = data_source.books_between_years(sys.argv[2], sys.argv[3])
+            print(years)
+        elif len(sys.argv) == 3:
+            years = data_source.books_between_years(sys.argv[2])
+            print(years)
+        elif len(sys.argv) == 2:
+            years = data_source.books_between_years()
+            print(years)
+        else:
+            print("Too many arguments provided, please see man to see command line interface")
+
+    elif sys.argv[1] == "man":
+        file = open("usage.txt", "r")
+        print(file.read())
+    else:
+        print("Command not found, please see man to see command line interface")
+
        
 if __name__ == '__main__':
     main()
